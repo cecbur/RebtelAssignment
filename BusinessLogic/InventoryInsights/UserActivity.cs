b2d20b0d9de4c268f6e3ffde6d5eab0d50c4a4e2
CB <cecilia_burman@hotmail.com>
2025-11-28 10:00:12 +0100

    UserActivity

---
+++ BusinessLogic/InventoryInsights/UserActivity.cs
@@ -0,0 +1,30 @@
+using BusinessModels;
+
+namespace BusinessLogic.InventoryInsights;
+
+public class UserActivity
+{
+    public UserActivity(LoanRepository loanRepository)
+    {
+        
+    }
+    
+    public PatronLoans[] PatronLoansOrderedByFrequency()
+    {
+        
+    }
+    
+    public class PatronLoans
+    {
+        public PatronLoans(Patron patron, Loan[] loans)
+        {
+            Patron = patron;
+            Loans = loans;
+        }
+
+        public int LoanCount => Loans.Count();
+        public Patron Patron { get;}
+        public Loan[] Loans { get;}
+    }
+    
+}
