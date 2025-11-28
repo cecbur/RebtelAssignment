7dd950e5f7208d3cd80189e3f0980f2a2916b8d8
CB <cecilia_burman@hotmail.com>
2025-11-28 07:49:36 +0100

    AssignmentStarters

--- BusinessLogic/AssignmentStarters.cs
+++ BusinessLogic/AssignmentStarters.cs
@@ -17,56 +17,29 @@ public class AssignmentStarters
     /// 2. Reverse a book title 
     public string ReverseTitle(string title)
     {
-        // Convert to char array, reverse, and convert back to string
         char[] charArray = title.ToCharArray();
         Array.Reverse(charArray);
         return new string(charArray);
     }
 
-    /// <summary>
-    /// Generates a string by repeating a book title a specified number of times
-    /// </summary>
-    /// <param name="title">The book title to repeat</param>
-    /// <param name="count">The number of times to repeat the title</param>
-    /// <returns>The title repeated count times</returns>
-    /// <exception cref="ArgumentNullException">Thrown when title is null</exception>
-    /// <exception cref="ArgumentException">Thrown when count is negative</exception>
-    public string GenerateTitleReplicas(string title, int count)
+    /// 3. Generates book title replicas
+    public string GenerateTitleRepetitions(string title, int count)
     {
-        if (title == null)
+        var repeated = "";
+        for (int i = 0; i < count; i++)
         {
-            throw new ArgumentNullException(nameof(title), "Title cannot be null");
+            repeated += title;
         }
 
-        if (count < 0)
-        {
-            throw new ArgumentException("Count must be non-negative", nameof(count));
-        }
-
-        if (count == 0)
-        {
-            return string.Empty;
-        }
-
-        // Use string.Create for better performance with large strings
-        return string.Concat(Enumerable.Repeat(title, count));
+        return repeated;
     }
 
-    /// <summary>
-    /// Lists all odd-numbered Book IDs from a collection
-    /// </summary>
-    /// <param name="bookIds">Collection of book IDs to filter</param>
-    /// <returns>Collection containing only odd-numbered book IDs</returns>
-    /// <exception cref="ArgumentNullException">Thrown when bookIds is null</exception>
-    public IEnumerable<int> GetOddNumberedBookIds(IEnumerable<int> bookIds)
+    /// 4. Lists odd-numbered Book IDs 
+    public void PrintOddNumberedBookIds0To100()
     {
-        if (bookIds == null)
-        {
-            throw new ArgumentNullException(nameof(bookIds), "Book IDs collection cannot be null");
-        }
-
-        // Use LINQ for clean, declarative filtering
-        // Modulo operator determines if number is odd
-        return bookIds.Where(id => id % 2 != 0);
+        var ids = Enumerable.Range(0, 100).ToArray();
+        var odd = ids.Where(id => id % 2 != 0);
+        Console.WriteLine(string.Join(Environment.NewLine, odd));
     }
+
 }
