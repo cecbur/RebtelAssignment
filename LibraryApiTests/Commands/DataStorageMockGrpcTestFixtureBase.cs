220d62f6cbbcfc4aef4de5d8f8fc670ec4621a87
CB <cecilia_burman@hotmail.com>
2025-11-30 10:43:48 +0100

    DataStorageMockGrpcTestFixtureBase

--- LibraryApiTests/Commands/DataStorageMockGrpcTestFixtureBase.cs
+++ LibraryApiTests/Commands/DataStorageMockGrpcTestFixtureBase.cs
@@ -30,7 +30,16 @@ public abstract class DataStorageMockGrpcTestFixtureBase
 
         var port = FindFreePortForTestServer();
         ServerAddress = $"http://localhost:{port}";
-        _testServer = await CreateAndStartTestGrpcServer(ServerAddress, port, MockLoanRepository.Object, MockBorrowingPatternRepository.Object, _mockGrpcServiceLogger.Object);
+
+        _testServer = new TestServerBuilder()
+            .WithMockedRepositories(
+                MockLoanRepository.Object,
+                MockBorrowingPatternRepository.Object,
+                _mockGrpcServiceLogger.Object)
+            .OnPort(port)
+            .Build();
+
+        await _testServer.StartAsync();
     }
 
     protected async Task TearDownGrpcServer()
@@ -42,45 +51,6 @@ public abstract class DataStorageMockGrpcTestFixtureBase
         }
     }
 
-    private static async Task<IHost> CreateAndStartTestGrpcServer(string serverAddress, int port,
-        ILoanRepository mockLoanRepository,
-        IBorrowingPatternRepository mockBorrowingPatternRepository,
-        ILogger<BusinessLogic.Services.BusinessLogicGrpcService> mockGrpcServiceLogger)
-    {
-        var hostBuilder = Host.CreateDefaultBuilder()
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseUrls(serverAddress);
-                webBuilder.ConfigureKestrel(options =>
-                {
-                    options.ListenLocalhost(port, o => o.Protocols = HttpProtocols.Http2);
-                });
-                webBuilder.ConfigureServices(services =>
-                {
-                    // Register mocked repositories
-                    services.AddSingleton(mockLoanRepository);
-                    services.AddSingleton(mockBorrowingPatternRepository);
-                    services.AddSingleton(mockGrpcServiceLogger);
-
-                    services.AddBusinessLogicGrpcClient(serverAddress);
-
-                    services.AddGrpc();
-                });
-                webBuilder.Configure(app =>
-                {
-                    app.UseRouting();
-                    app.UseEndpoints(endpoints =>
-                    {
-                        endpoints.MapBusinessLogicGrpcService();
-                    });
-                });
-            });
-
-        var testServer = hostBuilder.Build();
-        await testServer.StartAsync();
-        return testServer;
-    }
-
     private static int FindFreePortForTestServer()
     {
         var listener = new System.Net.Sockets.TcpListener(System.Net.IPAddress.Loopback, 0);
@@ -88,5 +58,76 @@ public abstract class DataStorageMockGrpcTestFixtureBase
         var port = ((System.Net.IPEndPoint)listener.LocalEndpoint).Port;
         listener.Stop();
         return port;
+    }
+
+    private class TestServerBuilder
+    {
+        private ILoanRepository? _loanRepository;
+        private IBorrowingPatternRepository? _borrowingPatternRepository;
+        private ILogger<BusinessLogic.Services.BusinessLogicGrpcService>? _grpcServiceLogger;
+        private int _port;
+        private string _serverAddress = null!;
+
+        public TestServerBuilder WithMockedRepositories(
+            ILoanRepository loanRepo,
+            IBorrowingPatternRepository borrowingRepo,
+            ILogger<BusinessLogic.Services.BusinessLogicGrpcService> grpcServiceLogger)
+        {
+            _loanRepository = loanRepo;
+            _borrowingPatternRepository = borrowingRepo;
+            _grpcServiceLogger = grpcServiceLogger;
+            return this;
+        }
+
+        public TestServerBuilder OnPort(int port)
+        {
+            _port = port;
+            _serverAddress = $"http://localhost:{port}";
+            return this;
+        }
+
+        public IHost Build()
+        {
+            if (_loanRepository == null || _borrowingPatternRepository == null || _grpcServiceLogger == null)
+            {
+                throw new InvalidOperationException("Mocked repositories must be configured before building the server.");
+            }
+
+            if (_port == 0)
+            {
+                throw new InvalidOperationException("Port must be configured before building the server.");
+            }
+
+            var hostBuilder = Host.CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseUrls(_serverAddress);
+                    webBuilder.ConfigureKestrel(options =>
+                    {
+                        options.ListenLocalhost(_port, o => o.Protocols = HttpProtocols.Http2);
+                    });
+                    webBuilder.ConfigureServices(services =>
+                    {
+                        // Register mocked repositories
+                        services.AddSingleton(_loanRepository);
+                        services.AddSingleton(_borrowingPatternRepository);
+                        services.AddSingleton(_grpcServiceLogger);
+
+                        services.AddBusinessLogicGrpcClient(_serverAddress);
+
+                        services.AddGrpc();
+                    });
+                    webBuilder.Configure(app =>
+                    {
+                        app.UseRouting();
+                        app.UseEndpoints(endpoints =>
+                        {
+                            endpoints.MapBusinessLogicGrpcService();
+                        });
+                    });
+                });
+
+            return hostBuilder.Build();
+        }
     }
 }
